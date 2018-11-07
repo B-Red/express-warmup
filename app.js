7ed83647048c28db804e396ed5e51a95de6ce927
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-07 15:28:57 -0700

    reference

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ const app = express()
 const data = require('./data.json')
 const port = 3000
 
+app.get('/', (req, res, next) => {
+    res.send({ message: 'try again' })
+})
+
 app.get('/cakes', (req, res, next) => {
-    console.log("test")
+    res.send({ cakes: data })
+})
+
+app.get('/cakes/:id', (req, res, next) => {
+    let id = req.params.id
+    let matchingCake = data.filter(cake => { 
+        return cake.id == id
+    })
+    if(!matchingCake.length){
+        res.status(404).send({ message: "Yo thats made up."})
+    } else {
+        res.send({ cake: matchingCake[0]})
+    }
+    
 })
 
 app.listen(port)
